78ee66cfc9a85a814f6c5be9075aee5644ea4b27
Dan Kramer <kramer425@gmail.com>
2017-08-16 07:30:41 -0400

    Use one method for all public calls

--- src/main/scala/com/gdax/client/PublicGDaxClient.scala
+++ src/main/scala/com/gdax/client/PublicGDaxClient.scala
@@ -3,7 +3,7 @@ package com.gdax.client
 import com.gdax.error._
 import com.gdax.models.{Book, GDaxProduct, Time}
 import com.gdax.models.ImplicitsReads._
-import play.api.libs.json.{JsError, JsSuccess, Json}
+import play.api.libs.json.{JsError, JsSuccess, Json, Reads}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -12,55 +12,23 @@ class PublicGDaxClient(url: String) extends GDaxClient(url) {
 
   def products(): Future[Either[ErrorCode, List[GDaxProduct]]] = {
     val uri = url + "/products"
-    ws.url(uri).get().map(response => {
-      if (isValidResponse(response.status)) {
-        logger.debug(s"Sent URI: $uri. Received response: ${response.body}")
-        Json.parse(response.body).validate[List[GDaxProduct]] match {
-          case success: JsSuccess[List[GDaxProduct]] => Right(success.value)
-          case JsError(e) => Left(InvalidJson(e.toString()))
-        }
-      } else {
-        logger.debug(s"Sent URI: $uri. Response Error: ${response.status}.")
-        Left(RequestError(response.status))
-      }
-    })
+    publicRequest[List[GDaxProduct]](uri)
   }
 
   def book(productId: String): Future[Either[ErrorCode, Book]] = {
     val uri = s"$url/products/$productId/book"
-    ws.url(uri).get().map(response => {
-      if (isValidResponse(response.status)) {
-        logger.debug(s"Sent URI: $uri. Received response: ${response.body}")
-        Json.parse(response.body).validate[Book] match {
-          case success: JsSuccess[Book] => Right(success.value)
-          case JsError(e) => Left(InvalidJson(e.toString()))
-        }
-      } else {
-        logger.debug(s"Sent URI: $uri. Response Error: ${response.status}.")
-        Left(RequestError(response.status))
-      }
-    })
+    publicRequest[Book](uri)
   }
 
+  //need implicit reader
+/*
   def time(): Future[Either[ErrorCode, Time]] = {
     val uri = url + "/time"
-    ws.url(uri).get().map(response => {
-      if (isValidResponse(response.status)) {
-        logger.debug(s"Sent URI: $uri. Received response: ${response.body}")
-        Json.parse(response.body).validate[Time] match {
-          case success: JsSuccess[Time] => Right(success.value)
-          case JsError(e) => Left(InvalidJson(e.toString()))
-        }
-      } else {
-        logger.debug(s"Sent URI: $uri. Response Error: ${response.status}.")
-        Left(RequestError(response.status))
-      }
-    })
+    publicRequest[Time](uri)
   }
+*/
 
-  //WIP
-  /*
-  private def publicRequest[A](uri: String): Future[Either[ErrorCode, A]] = {
+  private def publicRequest[A: Reads](uri: String): Future[Either[ErrorCode, A]] = {
     ws.url(uri).get().map(response => {
       if (isValidResponse(response.status)) {
         logger.debug(s"Sent URI: $uri. Received response: ${response.body}")
@@ -74,7 +42,6 @@ class PublicGDaxClient(url: String) extends GDaxClient(url) {
       }
     })
   }
-  */
 }
 
 object PublicGDaxClient {
